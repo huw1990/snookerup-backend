c669b6e0653569fc0c14c0c60a7d9a3faef550df
Huw Carpenter <huwcarpenter@gmail.com>
2022-09-07 21:45:08 +0100

    Initial commit

---
+++ src/main/java/com/huwdunnit/snookerupbackend/SnookerupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.huwdunnit.snookerupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnookerupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnookerupBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huwdunnit/snookerupbackend/SnookerupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huwdunnit.snookerupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnookerupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
